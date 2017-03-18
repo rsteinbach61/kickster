ba6c32bfe8589e22c4acad8f7081e6c97bf1b3f6
Richard Steinbach <steinbach.dick@gmail.com>
2017-03-18 15:22:13 -0700

    add header bar and start songlist

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -1,8 +1,19 @@
-var Trackster = {};
+
+$(document).ready(function() {
+  var Trackster = {};
+$("#searchbtn").click(function(){
+  //var titleString = $("#search_box").val();
+  Trackster.searchTracksByTitle($("#search_box").val());
+});
+
+
+
+
+
 
 /*
   Given an array of track data, create the HTML for a Bootstrap row for each.
-  Append each "row" to the container in the body to display all tracks. 
+  Append each "row" to the container in the body to display all tracks.
 */
 Trackster.renderTracks = function(tracks) {
 
@@ -13,5 +24,8 @@ Trackster.renderTracks = function(tracks) {
   Render the tracks given in the API query response.
 */
 Trackster.searchTracksByTitle = function(title) {
-
+  console.log($.ajax({
+    url: 'https://api.spotify.com/v1/search?type=track&q=' + title,
+  }));
 };
+});
